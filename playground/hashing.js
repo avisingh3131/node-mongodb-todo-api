c246d92b77ad1905ab79fa347da6a3204fff8359
Avishek singh <avisingh3131@gmail.com>
2018-08-23 19:23:53 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,48 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
-    id:20
-};
-var token=jwt.sign(data,'123abc');
-console.log(token);
 
-var decoded=jwt.verify(token,'123abc');
-console.log('decoded',decoded);
+var password='123abc!';
+
+// bcrypt.genSalt(10,(err,salt)=>{
+// bcrypt.hash(password,salt,(err,hash)=>{
+// console.log(hash);
+
+// });
+// });
+
+var hashedPassword='$2a$10$AmRwSdLW9eO3SS1zOSXvZuLfYOF6LGXlyyIQNvlM1jG3y6EFFEXa2';
+
+bcrypt.compare('123!',hashedPassword,(err,res)=>{
+console.log(res);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var data={
+//     id:20
+// };
+// var token=jwt.sign(data,'123abc');
+// console.log(token);
+
+// var decoded=jwt.verify(token,'123abc');
+// console.log('decoded',decoded);
 
 
 
